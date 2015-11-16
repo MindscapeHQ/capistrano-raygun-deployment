28742d6f94f53c599c77e247d2e799a574c8215b
Jamie Penney <jpenney@raygun.io>
2015-11-17 10:17:38 +1300

    Add :raygun_deployment_environments option to cap task
    Allows the environments that we should create deployments for to be
    configured.

--- example/config/deploy.rb
+++ example/config/deploy.rb
@@ -4,3 +4,4 @@ lock '3.4.0'
 set :application, 'my_app_name'
 set :repo_url, 'git@github.com:MindscapeHQ/capistrano-raygun-deployment.git'
 set :deploy_to, '/var/www/my_app_name'
+set :raygun_deployment_environments, [:production]
